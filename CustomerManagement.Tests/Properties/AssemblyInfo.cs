697f171413b48a42aab7d5bb304e5decde9d5557
Mario <mqs@melix.org>
2017-03-16 17:24:41 +0100

    first commit

---
+++ CustomerManagement.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomerManagement.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("eFront")]
+[assembly: AssemblyProduct("CustomerManagement.Tests")]
+[assembly: AssemblyCopyright("Copyright © eFront 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("46ad585f-6875-4174-8769-c47a46f5a308")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
